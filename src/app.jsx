6f78391b662f6f1009264451a16f5ea2ad6ab3bc
Tyrantz <raymanv3@gmail.com>
2016-04-04 10:40:10 +0000

    add styling, entry point for app, app functionality: persistent storage, add/delete, priority shifting

---
+++ src/app.jsx
@@ -0,0 +1,143 @@
+import React from 'react';
+import $ from 'jquery';
+require('bootstrap-webpack');
+require('./styles.scss');
+require('font-awesome-webpack');
+
+var colors = ['Edit me! Delete Me! Add More!'];
+var placeholder = document.createElement('li');
+placeholder.className = 'placeholder';
+/* localStorage.setItem('stored-recipes', JSON.stringify(names)); */
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    if(localStorage) {
+      // use the local storage to set the state
+      if (localStorage.getItem('temporary2')) {
+        // set the state here
+        this.state = {
+          colors: JSON.parse(localStorage.getItem('temporary2'))
+        };
+        console.log('this is what was in memory');
+        console.log(localStorage.getItem('temporary2'));
+      } else {
+        // there isnt anything stored there yet and we need to use a default value
+        console.log('there was nothing in memory');
+        this.state = {
+          colors: colors
+        };
+      }
+    } else {
+      // you cant use local storage not available
+    }
+    /* this.state = {
+       colors: colors
+       }; */
+  }
+  addItemToList = () => {
+    console.log(colors);
+    var tmp = this.state.colors;
+    tmp.push('New Item');
+    this.setState({
+      colors: tmp
+    });
+    localStorage.setItem('temporary2', JSON.stringify(tmp));
+    console.log(colors);
+  }
+  removeItemFromList = (id) => {
+    console.log('I will remove ' +  this.state.colors[id] );
+    var tmp = this.state.colors;
+    tmp.splice(id, 1);
+    this.setState({
+      colors: tmp
+    });
+    localStorage.setItem('temporary2', JSON.stringify(tmp));
+  }
+  render() {
+    return (
+      <div className="list-container">
+        <div className="list-title">Recipe Box</div>
+        <List data={this.state.colors} removeItem={this.removeItemFromList}/>
+        <div className="list-footer" onClick={this.addItemToList}>
+          <i className="add-button fa fa-plus-square"></i>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+class List extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {data: this.props.data};
+  }
+  dragStart = (e) => {
+    this.dragged = e.currentTarget;
+    e.dataTransfer.effectAllowed = 'move';
+
+    e.dataTransfer.setData('text/html', e.currentTarget);
+  }
+  dragEnd = () => {
+    this.dragged.style.display = 'block';
+    this.dragged.parentNode.removeChild(placeholder);
+
+    var data = this.state.data;
+    var from = Number(this.dragged.dataset.id);
+    var to = Number(this.over.dataset.id);
+    if(this.nodePlacement == 'after') to--;
+    data.splice(to, 0, data.splice(from, 1)[0]);
+    this.setState({data: data});
+  }
+  dragOver = (e) => {
+    e.preventDefault();
+    this.dragged.style.display = 'none';
+    if(e.target.className == 'placeholder') return;
+    this.over = e.target;
+    e.target.parentNode.insertBefore(placeholder, e.target);
+    var relY = e.clientY - this.over.offsetTop;
+    var height = this.over.offsetHeight / 2;
+    var parent = e.target.parentNode;
+
+    if(relY > height) {
+      this.nodePlacement = 'after';
+      parent.insertBefore(placeholder, e.target.nextElementSibling);
+    } else if (relY < height) {
+      this.nodePlacement = 'before';
+      parent.insertBefore(placeholder, e.target);
+    }
+  }
+  removeItemHandler = (e) => {
+    console.log(e.target.parentElement.id);
+    this.props.removeItem(e.target.parentElement.id);
+  }
+  onClickHandler = (e) => {
+    // handle the clicks
+    console.log('reveal information')
+    console.log($(e.target).text());
+  }
+  render() {
+    return (
+      <ul onDragOver={this.dragOver}>
+        {this.state.data.map(function(item, i) {
+           return <li
+                    data-id={i}
+                    key={i}
+                    draggable="true"
+                    onDragEnd={this.dragEnd}
+                    onDragStart={this.dragStart}
+                    onClick={this.onClickHandler}
+                    className="accordion-section"
+                  >
+        {item}<span className="edit-buttons pull-right" id={i}><button type="button" id={i} className="btn btn-success edit-list-item"><i className="fa fa-pencil"></i></button><button type="button" id={i} className="btn btn-danger pull-right delete-list-item" onClick={this.removeItemHandler}><i className="fa fa-trash"></i></button></span>
+                  </li>;
+         }, this)}
+      </ul>
+    );
+  }
+}
+
+
+export default App;
